4675cb449fbde003c45865eee30888e609ff251d
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-12-15 16:31:22 +0000

    inital commit

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static int numClients = 4;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartClients();
+        }
+
+        private static void StartClients()
+        {
+            int i;
+            string currentProcessName = Environment.CommandLine;
+            Process[] plist = new Process[numClients];
+
+            Console.WriteLine("Spawning client processes...\n");
+
+            if (currentProcessName.Contains(Environment.CurrentDirectory))
+            {
+                currentProcessName = currentProcessName.Replace(Environment.CurrentDirectory, String.Empty);
+            }
+
+            // Remove extra characters when launched from Visual Studio
+            currentProcessName = currentProcessName.Replace("\\", String.Empty);
+            currentProcessName = currentProcessName.Replace("\"", String.Empty);
+
+            for (i = 0; i < numClients; i++)
+            {
+                // Start 'this' program but spawn a named pipe client.
+                plist[i] = Process.Start(currentProcessName, "spawnclient");
+            }
+            while (i > 0)
+            {
+                for (int j = 0; j < numClients; j++)
+                {
+                    if (plist[j] != null)
+                    {
+                        if (plist[j].HasExited)
+                        {
+                            Console.WriteLine("Client process[{0}] has exited.",
+                                plist[j].Id);
+                            plist[j] = null;
+                            i--;    // decrement the process watch count
+                        }
+                        else
+                        {
+                            Thread.Sleep(250);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("\nClient processes finished, exiting.");
+        }
+    }
+}
