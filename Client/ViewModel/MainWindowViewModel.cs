fd48966220758018652a7474b76f4a463adce030
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-12-19 16:19:47 +0000

    add client main window view model

---
+++ Client/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Client.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private string _output;
+
+        public string Output
+        {
+            get { return _output; }
+            set
+            {
+                _output = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
